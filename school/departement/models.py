c6dee3c678ee01d6b265b32769e19b899b149e10
MonssifElkadi <elkadi.monssif@etu.uae.ac.ma>
2026-04-03 22:34:15 +0100

    final - push inxaelah , fix d'un bug au departement

--- school/departement/models.py
+++ school/departement/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Department(models.Model):
     name = models.CharField(max_length=100 , unique=True)
     description = models.TextField(blank=True)
+    head_teacher = models.ForeignKey('teacher.Teacher', on_delete=models.SET_NULL, null=True, blank=True, related_name='headed_departments')
 
     def __str__(self):
         return self.name
--- school/departement/views.py
+++ school/departement/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from .models import Department
 from home_auth.decorators import admin_required
+from teacher.models import Teacher
 
 @admin_required
 def departement_list(request):
@@ -10,27 +11,43 @@ def departement_list(request):
 
 @admin_required
 def add_department(request):
+    teachers = Teacher.objects.all()
     if request.method == 'POST':
         name = request.POST.get('name')
         description = request.POST.get('description')
+        head_teacher_id = request.POST.get('head_teacher')
+        
+        head_teacher = None
+        if head_teacher_id:
+             head_teacher = get_object_or_404(Teacher, pk=head_teacher_id)
+             
         Department.objects.create(
             name=name,
-            description=description
+            description=description,
+            head_teacher=head_teacher
         )
         messages.success(request, 'Department added successfully')
         return redirect('department_list')
-    return render(request,'departments/add-department.html')
+    return render(request,'departments/add-department.html', {'teachers': teachers})
 
 @admin_required
 def edit_department(request, pk):
     department = get_object_or_404(Department, pk=pk)
+    teachers = Teacher.objects.all()
     if request.method == 'POST':
         department.name = request.POST.get('name')
         department.description = request.POST.get('description')
+        head_teacher_id = request.POST.get('head_teacher')
+        
+        if head_teacher_id:
+             department.head_teacher = get_object_or_404(Teacher, pk=head_teacher_id)
+        else:
+             department.head_teacher = None
+             
         department.save()
         messages.success(request, 'Department updated successfully')
         return redirect('department_list')
-    return render(request, 'departments/edit-department.html', {'department': department})
+    return render(request, 'departments/edit-department.html', {'department': department, 'teachers': teachers})
 
 @admin_required
 def delete_department(request, pk):
